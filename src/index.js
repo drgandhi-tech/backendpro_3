a37d940b10577c158a7d94111f4e9390007ceeb2
Your Name <your.email@example.com>
2025-05-30 21:56:07 +0530

    mongodb database connected

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require('dotenv').config({path:'./env'})
+import donenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import express from "express";
+import connectDB from "./db/index";
+
+const app = express()
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+// (
+//     async () => {
+//         try{
+//           await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//           app.on ("error", (error) => {
+//             console.log("error:", error);
+//             throw error
+//           })
+//           app.listen(process.env.PORT, () =>{
+//             console.log(`app is listening port ${process.env.PORT}`);
+//           })
+
+//         }
+//         catch(error){
+//             console.error("ERROR:", error)
+//             throw err
+//         }
+        
+        
+//     })
+// ()
