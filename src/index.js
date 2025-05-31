7dc9c24a79a250449db158621967dab2a227d34b
Your Name <your.email@example.com>
2025-05-31 08:59:20 +0530

    api error handling

--- src/index.js
+++ src/index.js
@@ -10,7 +10,18 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is runinng at port: ${
+        process.env.PORT
+        }`);
+        
+    })
+})
+.catch((err) => {
+    console.log("mongodb connect failed!!", err)
+})
 // (
 //     async () => {
 //         try{
